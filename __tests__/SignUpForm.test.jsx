2cfef858ba589c014f75e12421bcbe7791ecdc9d
Sachin Gyandor <gyandors@gmail.com>
2024-06-02 02:21:31 +0530

    Implemented TDD and did Unit tests

---
+++ __tests__/SignUpForm.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import SignUpForm from '../src/components/Auth/SignUpForm';
+import { describe } from 'vitest';
+
+const component = (
+  <BrowserRouter>
+    <SignUpForm />
+  </BrowserRouter>
+);
+
+describe('Testing SignUp Component', () => {
+  test('Should render "Create new account"', () => {
+    render(component);
+
+    expect(
+      screen.getByText('Create new account', { exact: true })
+    ).toBeInTheDocument();
+  });
+
+  test('Should render "Login here"', () => {
+    render(component);
+
+    expect(
+      screen.getByText('login here', { exact: false })
+    ).toBeInTheDocument();
+  });
+});
